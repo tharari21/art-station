af749d2fc002a52b219db15ee997db09c9171b04
Tomer Harari <tharari21@gmail.com>
2022-09-17 18:28:38 -0400

    hoverable dropdown added

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -10,10 +10,20 @@ const Navbar = () => {
               Home
             </NavLink>
           </li>
-          <li className="navbar__item">
+          <li className="navbar__item navbar__item--hoverable">
             <NavLink className="navbar__link" to="/store">
               Supplies
             </NavLink>
+            <div className="navbar__dropdown">
+              <ul>
+                <li>
+                  <a>One</a>
+                </li>
+                <li>
+                  <a>Two</a>
+                </li>
+              </ul>
+            </div>
           </li>
           <li className="navbar__item">
             <NavLink className="navbar__link" to="/parties">
@@ -32,8 +42,16 @@ const Navbar = () => {
           </li>
         </ul>
         <ul className="navbar__menu navbar__menu--auth">
-          <li className="navbar__item">Login</li>
-          <li className="navbar__item">Sign Up</li>
+          <li className="navbar__item">
+            <NavLink className="navbar__link" to="/login">
+              Login
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink className="navbar__link" to="/register">
+              Sign Up
+            </NavLink>
+          </li>
         </ul>
       </div>
     </nav>
